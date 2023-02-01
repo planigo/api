eed8f6f8ec0e3db6b02e9cf83914c306028e7f70
awuzi <yahia.lamri@outlook.fr>
2023-02-02 00:41:23 +0100

    feat(auth) : move middlewares in appropriate folder

--- api/routes/auth.go
+++ api/routes/auth.go
@@ -2,8 +2,8 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
-	"net/http"
+	"planigo/api/middlewares"
+
 	"planigo/pkg/auth"
 )
 
@@ -11,19 +11,6 @@ func AuthRoutes(app fiber.Router, handler *auth.Handler) {
 	router := app.Group("/auth")
 
 	router.Post("/login", handler.Login())
-	router.Get("/me", requireLogin(handler.Session), handler.Me())
-	router.Get("/logout", requireLogin(handler.Session), handler.Logout())
-}
-
-func requireLogin(r *session.Store) func(c *fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		sess, err := r.Get(c)
-		if err != nil {
-			panic(err)
-		}
-		if sess.Get("uid") == nil {
-			return c.SendStatus(http.StatusUnauthorized)
-		}
-		return c.Next()
-	}
+	router.Get("/me", middlewares.RequireLogin(handler.Session), handler.Me())
+	router.Get("/logout", middlewares.RequireLogin(handler.Session), handler.Logout())
 }
