ec02c4c4c0cbfdeb63e9c6626fca64d9c8c361ce
Jessy <c.j91350@gmail.com>
2023-02-01 22:02:54 +0100

    feat: Data Fixtures v0.1.0

--- database/001-schema.sql
+++ database/001-schema.sql
@@ -9,13 +9,13 @@ CREATE TABLE `User`
     `is_email_verified` BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE `Store`
+CREATE TABLE `Shop`
 (
     `id`          VARCHAR(36) PRIMARY KEY NOT NULL,
     `name`        VARCHAR(255)            NOT NULL,
     `description` TEXT,
     `owner_id`    VARCHAR(36),
-    CONSTRAINT `fk_store_owner`
+    CONSTRAINT `fk_shop_owner`
         FOREIGN KEY (`owner_id`) REFERENCES `User` (`id`)
 );
 
@@ -25,10 +25,10 @@ CREATE TABLE `Service`
     `name`        VARCHAR(255)            NOT NULL,
     `description` TEXT,
     `price`       FLOAT,
-    `store_id`    VARCHAR(36),
     `duration`    INTEGER DEFAULT 60,
-    CONSTRAINT `fk_service_store`
-        FOREIGN KEY (`store_id`) REFERENCES `Store` (`id`)
+    `shop_id`    VARCHAR(36),
+    CONSTRAINT `fk_service_shop`
+        FOREIGN KEY (`shop_id`) REFERENCES `Shop` (`id`)
 );
 
 CREATE TABLE `Reservation`
@@ -49,47 +49,47 @@ CREATE TABLE `Hour`
     `start`    time,
     `end`      time,
     `day`      integer,
-    `store_id` VARCHAR(36),
-    CONSTRAINT `fk_hours_store`
-        FOREIGN KEY (`store_id`) REFERENCES `Store` (`id`)
+    `shop_id` VARCHAR(36),
+    CONSTRAINT `fk_hours_shop`
+        FOREIGN KEY (`shop_id`) REFERENCES `Shop` (`id`)
 );
 
-CREATE TRIGGER `insert_user`
-    BEFORE INSERT
-    ON `User`
-    FOR EACH ROW
-BEGIN
-    SET NEW.id = UUID();
-END;
+-- CREATE TRIGGER `insert_user`
+--     BEFORE INSERT
+--     ON `User`
+--     FOR EACH ROW
+-- BEGIN
+--     SET NEW.id = UUID();
+-- END;
 
-CREATE TRIGGER `insert_store`
-    BEFORE INSERT
-    ON `Store`
-    FOR EACH ROW
-BEGIN
-    SET NEW.id = UUID();
-END;
+-- CREATE TRIGGER `insert_shop`
+--     BEFORE INSERT
+--     ON `Shop`
+--     FOR EACH ROW
+-- BEGIN
+--     SET NEW.id = UUID();
+-- END;
 
-CREATE TRIGGER `insert_service`
-    BEFORE INSERT
-    ON `Service`
-    FOR EACH ROW
-BEGIN
-    SET NEW.id = UUID();
-END;
+-- CREATE TRIGGER `insert_service`
+--     BEFORE INSERT
+--     ON `Service`
+--     FOR EACH ROW
+-- BEGIN
+--     SET NEW.id = UUID();
+-- END;
 
-CREATE TRIGGER `insert_reservation`
-    BEFORE INSERT
-    ON `Reservation`
-    FOR EACH ROW
-BEGIN
-    SET NEW.id = UUID();
-END;
+-- CREATE TRIGGER `insert_reservation`
+--     BEFORE INSERT
+--     ON `Reservation`
+--     FOR EACH ROW
+-- BEGIN
+--     SET NEW.id = UUID();
+-- END;
 
-CREATE TRIGGER `insert_hours`
-    BEFORE INSERT
-    ON `Hour`
-    FOR EACH ROW
-BEGIN
-    SET NEW.id = UUID();
-END;
+-- CREATE TRIGGER `insert_hours`
+--     BEFORE INSERT
+--     ON `Hour`
+--     FOR EACH ROW
+-- BEGIN
+--     SET NEW.id = UUID();
+-- END;
