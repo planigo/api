8216f664e8ded3696534aa5e9a5f6521ff2c4a8f
Louis Genestier <louis.genestier@gmail.com>
2023-02-03 12:09:12 +0100

    feat(category): add category table + relation with shop

--- config/store/shop_store.go
+++ config/store/shop_store.go
@@ -27,7 +27,7 @@ func (store *ShopStore) FindShops() ([]entities.Shop, error) {
 
 	for rows.Next() {
 		var shopRow entities.Shop
-		if err := rows.Scan(&shopRow.Id, &shopRow.Name, &shopRow.Description, &shopRow.OwnerID); err != nil {
+		if err := rows.Scan(&shopRow.Id, &shopRow.Name, &shopRow.Description, &shopRow.OwnerID, &shopRow.CategoryID); err != nil {
 			return shops, err
 		}
 		shops = append(shops, shopRow)
@@ -44,7 +44,7 @@ func (store *ShopStore) FindShopById(shopId string) (entities.Shop, error) {
 
 	row := store.QueryRow("SELECT * FROM Shop WHERE id = ?;", shopId)
 
-	if err := row.Scan(&shop.Id, &shop.Name, &shop.Description, &shop.OwnerID); err != nil {
+	if err := row.Scan(&shop.Id, &shop.Name, &shop.Description, &shop.OwnerID, &shop.CategoryID); err != nil {
 		return shop, err
 	}
 
@@ -54,9 +54,9 @@ func (store *ShopStore) FindShopById(shopId string) (entities.Shop, error) {
 func (store *ShopStore) AddShop(newShop entities.ShopRequest) (entities.Shop, error) {
 	insertedShop := new(entities.Shop)
 
-	query := "INSERT INTO Shop (name, description, owner_id) VALUES (?, ?, ?) RETURNING id, name, description"
+	query := "INSERT INTO Shop (name, description, owner_id, category_id) VALUES (?, ?, ?, ?) RETURNING id, name, description, category_id"
 
-	if err := store.QueryRow(query, newShop.Name, newShop.Description, newShop.OwnerID).Scan(&insertedShop.Id, &insertedShop.Name, &insertedShop.Description); err != nil {
+	if err := store.QueryRow(query, newShop.Name, newShop.Description, newShop.OwnerID, newShop.CategoryID).Scan(&insertedShop.Id, &insertedShop.Name, &insertedShop.Description, &insertedShop.CategoryID); err != nil {
 		return *insertedShop, err
 	}
 
@@ -64,7 +64,7 @@ func (store *ShopStore) AddShop(newShop entities.ShopRequest) (entities.Shop, er
 }
 
 func (store *ShopStore) UpdateShop(shopId string, shopEdited entities.ShopRequest) (string, error) {
-	row, err := store.Exec("UPDATE Shop SET name = ?, description = ? WHERE id = ?;", shopEdited.Name, shopEdited.Description, shopId)
+	row, err := store.Exec("UPDATE Shop SET name = ?, description = ?, category_id = ? WHERE id = ?;", shopEdited.Name, shopEdited.Description, shopEdited.CategoryID, shopId)
 
 	if err != nil {
 		return "", err
